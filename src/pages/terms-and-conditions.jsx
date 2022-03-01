5aee34c66f7c432017da8812a64e888a8dc3e8d9
Laderlappen <elmervermeulen@gmail.com>
2022-03-01 10:39:18 +0200

    Added terms and conditions content

--- src/pages/terms-and-conditions.jsx
+++ src/pages/terms-and-conditions.jsx
@@ -1,34 +1,165 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 export default function TermsAndConditions({}) {
-  return (
-    <Layout>
-      <Header noWeb3={true} />
-      <div className="content">
-        <h1>Terms and Conditions</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Header noWeb3={true} />
+            <div className="content">
+                <h1>Terms and Conditions</h1>
+                <p>
+                    {" "}
+                    <strong>Version 1.0, last edited 28 Feb 2022</strong>
+                </p>
+                <p>
+                    <ul data-indent-level="1">
+                        <li>
+                            <p data-renderer-start-pos="50">
+                                I am an adult, over the age of 18, capable of
+                                making my own decisions, evaluating my own risks
+                                and engaging with others for mutual benefit.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="194">
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>{" "}
+                                and/or LEVR are extremely experimental and could
+                                enter into several failure modes.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="289">
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>{" "}
+                                and/or LEVR could fail technically through a
+                                software vulnerability.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="369">
+                                While{" "}
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>{" "}
+                                and/or LEVR have been thoroughly tested, bugs
+                                may have nonetheless snuck through.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="469">
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>{" "}
+                                and/or LEVR could fail due to an economic
+                                attack, the details of which might not even be
+                                suspected at the time of launch.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="603">
+                                The projects that{" "}
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>{" "}
+                                fund may turn out to be flawed technically or
+                                have economic attack vectors that make them
+                                infeasible.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="736">
+                                LEVR, and the projects funded by{" "}
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>
+                                , might never find profitable returns.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="819">
+                                I will not hold the creators of this autonomous
+                                code liable for damages or losses. Even if I
+                                did, they will be unlikely to have the resources
+                                to settle.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="977">
+                                ETH deposited on the{" "}
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>{" "}
+                                interface will be held in a variety of smart
+                                contracts, which no one has direct control over,
+                                but may also be technically flawed.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="1140">
+                                I agree{" "}
+                                <a
+                                    title="http://levr.ly"
+                                    href="http://levr.ly/"
+                                    data-renderer-mark="true"
+                                >
+                                    levr.ly
+                                </a>{" "}
+                                may track anonymized data about my interactions
+                                with the sale.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="1223">
+                                Entering ETH into the sale is irrevocable,
+                                therefore, I will have to sell my LEVR into
+                                whatever is on offer on the secondary market at
+                                that time.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="1372">
+                                US citizens and residents are strictly
+                                prohibited from this sale.
+                            </p>
+                        </li>
+                        <li>
+                            <p data-renderer-start-pos="1442">
+                                I am not a citizen or resident of the&nbsp;USA.
+                            </p>
+                        </li>
+                    </ul>
+                </p>
+            </div>
+        </Layout>
+    );
 }
